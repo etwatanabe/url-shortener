a1afda8c99cf8c8fafff64139fc140188ae09240
Eduardo Takeshi <wattksh@gmail.com>
2025-06-27 20:28:16 -0300

    refactor: remove optional userId field from CreateUrlDto and UpdateUrlDto

--- apps/url-shortener-service/src/dto/create-url.dto.ts
+++ apps/url-shortener-service/src/dto/create-url.dto.ts
@@ -1,13 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsUrl, IsOptional, IsString } from 'class-validator';
+import { IsUrl } from 'class-validator';
 
 export class CreateUrlDto {
   @ApiProperty({ example: 'http://example.com' })
   @IsUrl()
   longUrl: string;
-
-  @ApiProperty({ required: false })
-  @IsOptional()
-  @IsString()
-  userId?: string;
 }
--- apps/url-shortener-service/src/dto/update-url.dto.ts
+++ apps/url-shortener-service/src/dto/update-url.dto.ts
@@ -1,12 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsUrl } from 'class-validator';
+import { IsUrl } from 'class-validator';
 
 export class UpdateUrlDto {
   @ApiProperty({ example: 'http://new-example.com' })
   @IsUrl()
   longUrl: string;
-
-  @ApiProperty()
-  @IsString()
-  userId: string;
 }
