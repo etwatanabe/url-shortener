b9bdd76514304565063adbe58b4d8026f6f7cf50
Eduardo Takeshi <wattksh@gmail.com>
2025-06-27 01:37:29 -0300

    refactor: simplify PrismaService by extending PrismaClient directly

--- libs/prisma/src/prisma.service.ts
+++ libs/prisma/src/prisma.service.ts
@@ -2,18 +2,15 @@ import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService implements OnModuleInit, OnModuleDestroy {
-  private readonly prisma: PrismaClient;
-
-  constructor() {
-    this.prisma = new PrismaClient();
-  }
-
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   async onModuleInit(): Promise<void> {
-    await this.prisma.$connect();
+    await this.$connect();
   }
 
   async onModuleDestroy(): Promise<void> {
-    await this.prisma.$disconnect();
+    await this.$disconnect();
   }
 }
