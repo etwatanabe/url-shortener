69f2650b7b212efcbfbef7ae884485f1e80c75bf
Eduardo Takeshi <wattksh@gmail.com>
2025-06-27 02:11:17 -0300

    feat: add API documentation for URL shortening and redirection endpoints

--- apps/url-shortener-service/src/url-shortener-service.controller.ts
+++ apps/url-shortener-service/src/url-shortener-service.controller.ts
@@ -9,7 +9,10 @@ import {
 } from '@nestjs/common';
 import { UrlShortenerServiceService } from './url-shortener-service.service';
 import { CreateUrlDto } from './dto/create-url.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { UrlResponseDto } from './dto/url-response.dto';
 
+@ApiTags('urls')
 @Controller()
 export class UrlShortenerServiceController {
   constructor(
@@ -17,12 +20,20 @@ export class UrlShortenerServiceController {
   ) {}
 
   @Post('shorten')
+  @ApiOperation({ summary: 'Create a short URL' })
+  @ApiResponse({
+    status: 201,
+    description: 'URL shortened successfully',
+    type: UrlResponseDto,
+  })
   async shorten(@Body() createUrlDto: CreateUrlDto) {
     return this.urlShortenerService.create(createUrlDto);
   }
 
   @Get(':shortUrl')
   @Redirect()
+  @ApiOperation({ summary: 'Redirects to the original URL' })
+  @ApiResponse({ status: 302, description: 'Redirects to the original URL' })
   async redirect(@Param('shortUrl') shortUrl: string) {
     const longUrl = await this.urlShortenerService.redirect(shortUrl);
     if (!longUrl) throw new NotFoundException();
